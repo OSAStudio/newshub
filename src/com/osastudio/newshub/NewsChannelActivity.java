2d83c95a2216eebcf91587b47b4ff8d2ae10e589
Xue Ru Jin <xuerujin@gmail.com>
2013-07-19 21:57:46 +0800

    created android project framework

---
+++ src/com/osastudio/newshub/NewsChannelActivity.java
@@ -0,0 +1,14 @@
+package com.osastudio.newshub;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NewsChannelActivity extends Activity {
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.news_channel);
+   }
+
+}
