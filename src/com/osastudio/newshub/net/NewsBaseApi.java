d91e7c5bb32833a9668d56f43f0730d24781ad3d
XUE <arm@XBUNTU.(none)>
2013-07-22 16:30:03 +0800

    added api to get stream from server

--- src/com/osastudio/newshub/net/NewsBaseApi.java
+++ src/com/osastudio/newshub/net/NewsBaseApi.java
@@ -1,6 +1,7 @@
 package com.osastudio.newshub.net;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
@@ -8,7 +9,9 @@ import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
 import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.HTTP;
@@ -96,6 +99,33 @@ public class NewsBaseApi {
          // e.printStackTrace();
       }
 
+      return null;
+   }
+   
+   public static InputStream getStream(String url) {
+      try {
+	      HttpClient httpClient = new DefaultHttpClient();
+	      HttpGet httpGet = new HttpGet(url);
+      
+         HttpResponse httpResponse = httpClient.execute(httpGet);
+         if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            InputStream is = httpResponse.getEntity().getContent();
+            Utils.logi(TAG, "getFile() URL=" + url 
+                  + " length=" + httpResponse.getEntity().getContentLength());
+            if (is != null) {
+               return is;
+            }
+         }
+      } catch (ClientProtocolException e) {
+//         e.printStackTrace();
+      } catch (IllegalArgumentException e) {
+//         e.printStackTrace();
+      } catch (IllegalStateException e) {
+//         e.printStackTrace();
+      } catch (IOException e) {
+//         e.printStackTrace();
+      }
+      
       return null;
    }
 
