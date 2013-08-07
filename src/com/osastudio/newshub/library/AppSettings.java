ae67710a88ef5a884c36c87647a625fa741361ee
Xue Ru Jin <xuerujin@gmail.com>
2013-08-07 14:20:21 +0800

    addedd preference manager to process app settings

---
+++ src/com/osastudio/newshub/library/AppSettings.java
@@ -0,0 +1,12 @@
+package com.osastudio.newshub.library;
+
+public interface AppSettings {
+
+   public static final int FONT_SIZE_BIG = 20;
+   public static final int FONT_SIZE_NORMAL = 18;
+   public static final int FONT_SIZE_SMALL = 16;
+   public static final int DEFAULT_FONT_SIZE = FONT_SIZE_NORMAL;
+   
+   public static final boolean AUTO_LOADING_PICTURE = true;
+   
+}
