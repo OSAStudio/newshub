402d9aceb1934e66fa2a1598ac3bebb541cf07b8
XUE <arm@XBUNTU.(none)>
2013-08-28 09:23:08 +0800

    fixed bug settings top bar should not be scrolled with content

--- src/com/osastudio/newshub/data/NewsMessageList.java
+++ src/com/osastudio/newshub/data/NewsMessageList.java
@@ -8,9 +8,6 @@ import com.osastudio.newshub.data.base.NewsItemList;
 
 public class NewsMessageList extends NewsItemList<NewsMessage> {
    
-   private static final boolean DEBUG = true;
-   private static boolean TEST = false;
-
    public NewsMessageList(JSONObject jsonObject) {
       super(jsonObject);
 
@@ -38,33 +35,6 @@ public class NewsMessageList extends NewsItemList<NewsMessage> {
 
          }
       }
-      
-      //XTEST
-//      if (DEBUG) {
-//         NewsMessage msg = new NewsMessage();
-//         msg.setId("15");
-//         msg.setType(1);
-//         msg.setContent("鏂板鏈熷紑濮嬶紝鑷村闀夸滑鐨勪竴灏佷俊");
-//         msg.setUserId("10070");
-//         msg.setUserName("灏忚彶");
-//         this.list.add(msg);
-//         
-//         msg = new NewsMessage();
-//         msg.setId("1");
-//         msg.setType(4);
-//         msg.setContent("鍙戞帢瀛╁瓙鐨勬綔鍔涘氨鏄煿鍏诲ぉ鎵�);
-//         msg.setUserId("10070");
-//         msg.setUserName("灏忚彶");
-//         this.list.add(msg);
-//         
-//         msg = new NewsMessage();
-//         msg.setId("88");
-//         msg.setType(5);
-//         msg.setContent("璋佷簡瑙ｆ垜鐨勮鐭�);
-//         msg.setUserId("10070");
-//         msg.setUserName("灏忚彶");
-//         this.list.add(msg);
-//      }
    }
 
 }
